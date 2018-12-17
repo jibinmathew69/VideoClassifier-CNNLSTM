e31608ef1a5371cbdb136f792617093d2eb0c1c7
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-17 15:54:17 +0530

    Add debug for generator

--- keras_video_classifier/library/recurrent_networks.py
+++ keras_video_classifier/library/recurrent_networks.py
@@ -205,6 +205,13 @@ class VGG16BidirectionalLSTMVideoClassifier(object):
         train_num_batches = len(Xtrain) // BATCH_SIZE
         test_num_batches = len(Xtest) // BATCH_SIZE
 
+        print("Debug1")
+        print(len(Xtrain))
+        print(len(Xtest))
+        print(train_num_batches)
+        print(test_num_batches)
+        print("Debug1 finish")
+
         checkpoint = ModelCheckpoint(filepath=weight_file_path, save_best_only=True)
         history = model.fit_generator(generator=train_gen, steps_per_epoch=train_num_batches,
                                       epochs=NUM_EPOCHS,
