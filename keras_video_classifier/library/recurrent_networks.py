a4fbb939ce7b26463301030e876e70508126ccf8
Jibin Mathew <jibinmathew.69@gmail.com>
2018-12-18 15:42:25 +0530

    Debug

--- keras_video_classifier/library/recurrent_networks.py
+++ keras_video_classifier/library/recurrent_networks.py
@@ -53,7 +53,7 @@ class VGG16BidirectionalLSTMVideoClassifier(object):
         model.add(Bidirectional(LSTM(units=HIDDEN_UNITS, return_sequences=True),
                                 input_shape=(self.expected_frames, self.num_input_tokens)))
         model.add(Bidirectional(LSTM(10)))
-        model.add(Dense(512, activation='relu'))
+        model.add(Dense(64, activation='relu'))
         model.add(Dropout(0.5))
 
         model.add(Dense(self.nb_classes))
